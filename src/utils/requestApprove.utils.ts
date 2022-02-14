b46f2c8ef66a05458b99b505ee58818a7d281972
MatthiasWanner <matthias.wanner@gmail.com>
2022-02-14 15:15:37 +0100

    :sparkles: (approve) New approve logic to manage jEURO Payment

--- src/utils/requestApprove.utils.ts
+++ src/utils/requestApprove.utils.ts
@@ -1,10 +1,13 @@
 import {
+  approveErc20,
   approveErc20ForProxyConversion,
   checkErc20Allowance,
+  hasErc20Approval,
   utils as paymentUtils
 } from '@requestnetwork/payment-processor';
 import { AnyToERC20PaymentDetector } from '@requestnetwork/payment-detection';
 import { IRequestData } from '@requestnetwork/types/dist/client-types';
+import { ExtensionTypes } from '@requestnetwork/types';
 import { IWindowEthereum } from '../../types';
 import { checkConnectedNetwork, getWalletAddress } from './ethereum.utils';
 import { providers } from 'ethers';
@@ -13,6 +16,7 @@ import {
   getCurrencyInfos,
   prepareCalculateMaxToSpendArgs
 } from './prepareRequest.utils';
+import { getPaymentNetwork } from '.';
 
 export const hasAllowance = async (
   request: IRequestData,
@@ -28,31 +32,36 @@ export const hasAllowance = async (
 
   const [walletAddress] = await getWalletAddress(ethereum);
 
-  const proxyAddress = paymentUtils.getProxyAddress(
-    request,
-    AnyToERC20PaymentDetector.getDeploymentInformation
-  );
+  const paymentNetwork = getPaymentNetwork(request);
 
-  const maxToSpendArgs = await prepareCalculateMaxToSpendArgs(
-    currencyInfos,
-    request.currency
-  );
+  if (paymentNetwork === ExtensionTypes.ID.PAYMENT_NETWORK_ANY_TO_ERC20_PROXY) {
+    const proxyAddress = paymentUtils.getProxyAddress(
+      request,
+      AnyToERC20PaymentDetector.getDeploymentInformation
+    );
 
-  const maxToSpend = calculateMaxToSpend(
-    +request.expectedAmount,
-    +feeAmount,
-    maxToSpendArgs.rate,
-    maxToSpendArgs.decimals,
-    3
-  );
+    const maxToSpendArgs = await prepareCalculateMaxToSpendArgs(
+      currencyInfos,
+      request.currency
+    );
 
-  return await checkErc20Allowance(
-    walletAddress,
-    proxyAddress,
-    provider,
-    currencyInfos.value.toLowerCase(),
-    maxToSpend
-  );
+    const maxToSpend = calculateMaxToSpend(
+      +request.expectedAmount,
+      +feeAmount,
+      maxToSpendArgs.rate,
+      maxToSpendArgs.decimals,
+      3
+    );
+
+    return await checkErc20Allowance(
+      walletAddress,
+      proxyAddress,
+      provider,
+      currencyInfos.value.toLowerCase(),
+      maxToSpend
+    );
+  }
+  return await hasErc20Approval(request, walletAddress, provider);
 };
 
 export const approveERC20Transactions = async (
@@ -67,11 +76,16 @@ export const approveERC20Transactions = async (
 
   const provider = new providers.Web3Provider(ethereum);
 
-  const tx = await approveErc20ForProxyConversion(
-    request,
-    currencyInfos.value.toLowerCase(),
-    provider
-  );
+  const paymentNetwork = getPaymentNetwork(request);
+  if (paymentNetwork === ExtensionTypes.ID.PAYMENT_NETWORK_ANY_TO_ERC20_PROXY) {
+    const tx = await approveErc20ForProxyConversion(
+      request,
+      currencyInfos.value.toLowerCase(),
+      provider
+    );
 
-  await tx.wait(1);
+    await tx.wait(1);
+  }
+  const approvalTx = await approveErc20(request, provider);
+  await approvalTx.wait(1);
 };
